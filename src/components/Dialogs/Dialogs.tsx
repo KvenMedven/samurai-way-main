45bbbe7e1b65d84b3717e6af1bc449a7e6d0af74
Artur_Islakaev <artur_islakaev@mail.ru>
2023-03-06 16:04:03 +0500

    26

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,12 +3,10 @@ import s from "./Dialogs.module.css"
 import {NavLink} from "react-router-dom";
 
 type propsDialogsType = {}
-
 type propsDialogType = {
     name: string
-    id: string
+    id: number
 }
-
 type MessagePropsType = {
     message: string
 }
@@ -29,24 +27,33 @@ const Message = (props: MessagePropsType) => {
 }
 
 export const Dialogs = (props: propsDialogsType) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrew'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'},
+    ]
+    let messages = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your it-kamasutra'},
+        {id: 3, message: 'You'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yo'},
+        {id: 6, message: 'Yo'},
+    ]
+
+    let dialogsElements = dialogsData.map(d => <DialogItem name={d.name} id={d.id}/>)
+    let messageElements = messages.map(m => <Message message={m.message}/>)
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={'Dimych'} id={"1"}/>
-                <DialogItem name={'Andrey'} id={"2"}/>
-                <DialogItem name={'Sveta'} id={"3"}/>
-                <DialogItem name={'Sasha'} id={"4"}/>
-                <DialogItem name={'Viktor'} id={"5"}/>
-                <DialogItem name={'Valera'} id={"6"}/>
-
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message  message={"Hi"}/>
-                <Message  message={"How is your it-kamasutra"}/>
-                <Message  message={"Yo"}/>
-                <Message  message={"Yo"}/>
-                <Message  message={"Yo"}/>
-
+                {messageElements}
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,20 +3,25 @@ import s from './MyPosts.module.css';
 import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
+    let posts = [
+        {id: 1, message: 'Hi, how are you?',likesCount:12},
+        {id: 2, message: 'How is your it-kamasutra',likesCount: 11},
+    ]
+    let postsElements = posts.map(p=><Post message={p.message} likesCount={p.likesCount}/>)
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
                     <textarea></textarea>
-                </div>
+                </div>b
                 <div>
                     <button>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={"Hi, how are you?"} likesCount={11}/>
-                <Post message={"It's my first post"} likesCount={231}/>
+                {postsElements}
             </div>
         </div>
     )
